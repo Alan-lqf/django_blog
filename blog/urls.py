41d8e152f1492c57972e9c5c2596e2b71ab8b61b
Alan <1993826783@qq.com>
2018-04-16 23:35:21 +0800

    settings.py//ROOT_URLCONF='myblog.urls'

--- blog/urls.py
+++ blog/urls.py
@@ -3,14 +3,13 @@
 # @Author: 
 # @Date  : 2018/4/15
 # @Desc  :
-# from django.urls import path   2.0版没有path呀？？？
-from django.contrib import admin
-# from django.conf.urls import url
+
+
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    # path('admin/', admin.site.urls)
+    path('', views.index, name='index'),  # 注意2.0必须在后边添加name  # 2.0必须添加app_name app_name = 'index'
+
 ]
